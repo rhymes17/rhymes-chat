37b6df4b6626efcee31a3502ddc6f6daa44d028b
rhymes17 <devrahul1709@gmail.com>
2024-11-05 02:27:40 +0530

    fix: move middleware to route page from server.js

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getMessages, sendMessage } from "../controllers/message.controller.js";
+import protectedRoutes from "../middlewares/protectedRoutes.js";
 
 const router = express.Router();
 
-router.get("/:id", getMessages);
-router.post("/send/:id", sendMessage);
+router.get("/:id", protectedRoutes, getMessages);
+router.post("/send/:id", protectedRoutes, sendMessage);
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import protectedRoutes from "./middlewares/protectedRoutes.js";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", protectedRoutes, messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
