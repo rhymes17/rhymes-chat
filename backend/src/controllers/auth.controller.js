73e6060839a6d6ad8783e810b39e894efae444f3
rhymes17 <devrahul1709@gmail.com>
2024-11-04 23:22:07 +0530

    chore: backend initial folder setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+// @desc    Login a User
+// @route   POST /auth/login
+// @access  PUBLIC
+const loginUser = (req, res) => {
+    res.send("Hello from controller")
+}
+
+export {loginUser}
---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  connectToMongoDB();
+});
