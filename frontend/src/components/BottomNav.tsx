876395e1f6a6802700622fad8ae749d127d16559
rhymes17 <devrahul1709@gmail.com>
2024-11-07 03:07:37 +0530

    chore: scroll to bottom in conversation page

--- frontend/src/components/BottomNav.tsx
+++ frontend/src/components/BottomNav.tsx
@@ -33,7 +33,7 @@ const BottomNav = () => {
           onClick={() => navigate("/")}
         />
 
-        <div className="flex-1 flex bg-black items-center justify-center gap-1 py-2 rounded-3xl">
+        <div className="flex-1 flex bg-black items-center justify-center gap-1 py-2 rounded-3xl cursor-pointer">
           <IoMdAdd className="text-white text-lg font-bold" />
           <h3 className="text-white text-[0.8rem]">New Chat</h3>
         </div>
@@ -62,5 +62,5 @@ const BottomNavIcon = ({
   normalIcon: React.ReactNode;
   onClick: () => void;
 }) => {
-  return <div onClick={onClick}>{isActive ? activeIcon : normalIcon}</div>;
+  return <div className="cursor-pointer" onClick={onClick}>{isActive ? activeIcon : normalIcon}</div>;
 };
