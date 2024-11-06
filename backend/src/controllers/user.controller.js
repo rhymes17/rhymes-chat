ecc1effe0b6c0318779a5fc83d42fb4101eacb47
rhymes17 <devrahul1709@gmail.com>
2024-11-07 03:08:46 +0530

    chore: add get individual user fn to controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -23,3 +23,23 @@ export const getUsers = async (req, res) => {
     return res.status(500).json({ error: "Internal server error" });
   }
 };
+
+// @desc    Get users for sidebar
+// @route   GET /api/users/:id
+// @access  PRIVATE
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id).select("-password");
+    if (!user) {
+      return res.status(400).json({ error: "Could not get users" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Users fetched successfully", data: user });
+  } catch (error) {
+    console.log("Could not get the users", error.message);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
